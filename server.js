9a8c3813afd3cb48a2d3fe293480af499f0e74cc
vmidor <76964172+vmidor@users.noreply.github.com>
2021-08-13 18:46:09 -0400

    routes added

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
 
+//Connet Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
 
 const PORT = process.env.PORT || 5000;
 
