c26f3edb22eeb08bd7a584cd407c23176c638634
vmidor <76964172+vmidor@users.noreply.github.com>
2021-08-18 17:05:44 -0400

    added schema

--- models/Profile.js
+++ models/Profile.js
@@ -27,4 +27,83 @@ const ProfileSchema = new mongoose.Schema({
   githubuseername: {
     type: String
   },
+
+  experience: [{
+    title: {
+      type: String,
+      required: true
+    },
+    company: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String
+    },
+    from: {
+      type: Date,
+      required: true
+    },
+    to: {
+      type: Date
+    },
+    current: {
+      type: Boolean,
+      default: false
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }],
+
+  education: [{
+    school: {
+      type: String,
+      required: true
+    },
+    degree: {
+      type: String,
+      required: true
+    },
+    fieldofstudy: {
+      type: String,
+      required: true
+    },
+    from: {
+      type: Date,
+      required: true
+    },
+    to: {
+      type: Date
+    },
+    current: {
+      type: Boolean,
+      default: false
+    },
+    description: {
+      type: String,
+    }
+  }],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 })
