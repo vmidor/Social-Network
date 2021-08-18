3112ede5c032e54df60f09f0e68da5dbea8be099
vmidor <76964172+vmidor@users.noreply.github.com>
2021-08-18 16:56:30 -0400

    schema in profile.js

---
+++ models/Profile.js
@@ -0,0 +1,30 @@
+const mongoose = ('mongoose');
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubuseername: {
+    type: String
+  },
+})
