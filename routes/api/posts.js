905f4d3f029eb21b5ca62c74d459b634c5a8972e
vmidor <76964172+vmidor@users.noreply.github.com>
2021-08-26 19:38:12 -0400

    post by ID

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -52,6 +52,23 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+// @route    GET api/posts/:id
+// @desc     Get post by ID
+// @access   Private
+router.get('/:id', auth, checkObjectId('id'), async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
 
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router; 
