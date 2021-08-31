d442b1896f92a3f8c0dea0f4b1f6d2874a03ffbc
vmidor <76964172+vmidor@users.noreply.github.com>
2021-08-31 13:48:50 -0400

    alert and registred updated

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+import Register from './components/layout/auth/Register';
+import Login from './components/layout/auth/Login';
 import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
