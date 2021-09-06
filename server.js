f39d72575de3e6e0175bbd98aba4daba849fdf94
vmidor <76964172+vmidor@users.noreply.github.com>
2021-09-06 19:56:08 -0400

    updated

--- server.js
+++ server.js
@@ -1,34 +1,23 @@
-const express = require('express');
-const path = require('path');
 
-// Database
+const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
 connectDB();
 
 
-// Middleware
 app.use(express.json());
 
+app.get('/', (req, res) => res.send('API Running'));
 
-// Define routes
+
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-if (process.env.NODE_ENV === 'production') {
-
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
-
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
