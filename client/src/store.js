b93b82cf1d9495578ea3a9eec68d5aa1f51bafff
vmidor <76964172+vmidor@users.noreply.github.com>
2021-09-06 17:13:57 -0400

    post, comments, prepare

--- client/src/store.js
+++ client/src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import setAuthToken from './utils/setAuthToken';
 
 const initialState = {};
 
@@ -13,4 +14,17 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
+
+let currentState = store.getState();
+
+store.subscribe(() => {
+  let previousState = currentState;
+  currentState = store.getState();
+
+  if (previousState.auth.token !== currentState.auth.token) {
+    const token = currentState.auth.token
+    setAuthToken(token);
+  }
+
+});
 export default store;
